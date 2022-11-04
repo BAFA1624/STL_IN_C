7646618ae808461abb19826d95d9e3c0a6b5aff5
Ben Andrews <benafandrews@gmail.com>
2022-11-04 23:24:00 +0000

    Separated span_t & cspan_t with respective creation functions.

--- include/range.h
+++ include/range.h
@@ -2,6 +2,7 @@
 #define RANGE_H
 
 #include <stdint.h>
+#include <stdio.h>
 
 // Non-owning reference to span of elements.
 // Invalidated if referenced data goes out-of-scope.
@@ -10,21 +11,23 @@ typedef struct CONTIGUOUS_SPAN
     void *  begin;
     void *  end;
     int64_t step;
-} cspan_t;
+} span_t;
 
 typedef struct CONST_CONTIGUOUS_SPAN
 {
     const void *  begin;
     const void *  end;
     const int64_t step;
-} ccspan_t;
+} cspan_t;
+
+
+void print_span( span_t span );
 
 // Create cspan_t
-cspan_t create_cspan( void * const begin, const int64_t n, const int64_t step );
+span_t create_span( void * const begin, const int64_t n, const int64_t step );
 
-ccspan_t create_ccspan( const void * const begin, const int64_t n,
-                        const int64_t step );
+cspan_t create_cspan( const void * const begin, const int64_t n,
+                      const int64_t step );
 
 // void* foreach(
-
 #endif // RANGE_H
--- src/range.c
+++ src/range.c
@@ -1,8 +1,24 @@
 #include "range.h"
 
+void
+print_span( span_t span ) {
+    printf( "span{ begin: %p, end: %p, step: %lld }\n", span.begin, span.end,
+            span.step );
+}
+
+span_t
+create_span( void * const begin, const int64_t n, const int64_t step ) {
+    const span_t result = {
+        .begin = begin,
+        .end = ( void * ) ( ( ( char * ) begin ) + ( n * step ) ),
+        .step = step,
+    };
+    return result;
+}
+
 cspan_t
-create_cspan( void * const begin, const int64_t n, const int64_t step ) {
-    void * const tmp = ( void * ) ( ( char * ) begin + ( n * step ) );
+create_cspan( const void * const begin, const int64_t n, const int64_t step ) {
+    const void * const tmp = ( void * ) ( ( char * ) begin + ( n * step ) );
 
     const cspan_t result = {
         .begin = begin,
@@ -10,18 +26,6 @@ create_cspan( void * const begin, const int64_t n, const int64_t step ) {
         .step = step, // Extra comma for clang-format
     };
 
-    return result;
-}
-
-ccspan_t
-create_ccspan( const void * const begin, const int64_t n, const int64_t step ) {
-    const void * const tmp = ( void * ) ( ( char * ) begin + ( n * step ) );
-
-    ccspan_t result = {
-        .begin = begin,
-        .end = tmp,
-        .step = step,
-    };
     return result;
 }
 
